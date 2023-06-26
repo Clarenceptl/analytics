acf477f34b7f68b932c5ef191fa68cbbd4db4b7c
Clacla <potel.clarence@gmail.com>
2023-06-27 00:54:32 +0200

    generate module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 10
-    })
+    }),
+    AuthModule,
+    UserModule
   ],
   controllers: [],
   providers: [
