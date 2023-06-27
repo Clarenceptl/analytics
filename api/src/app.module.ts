48494733ad9455037e84ca16b1c337042040dafc
Clacla <potel.clarence@gmail.com>
2023-06-27 14:40:02 +0200

    connect mongo to api + Jwt module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot(process.env.DATABASE_MONGO_URL),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JSON_WEB_TOKEN_SECRET,
+      signOptions: {
+        expiresIn: '1d'
+      }
+    }),
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 10
@@ -21,4 +31,9 @@ import { UserModule } from './user/user.module';
     }
   ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log('AppModule constructor');
+    console.log('process.env.DATABASE_MONGO_URL', process.env.DATABASE_MONGO_URL);
+  }
+}
