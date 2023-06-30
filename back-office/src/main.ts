d4098ac72fd0630c6333030c572ce1ffbc4148bf
Clacla <potel.clarence@gmail.com>
2023-06-30 14:54:37 +0200

    fix toast

--- back-office/src/main.ts
+++ back-office/src/main.ts
@@ -1,12 +1,15 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
+import ToastPlugin from 'vue-toast-notification';
 
 import '@/assets/main.css';
+import 'vue-toast-notification/dist/theme-sugar.css';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
+app.use(ToastPlugin);
 app.use(createPinia());
 app.use(router);
 
