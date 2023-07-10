f747f746ee0af88a1bbda02df81fab2dde986977
Clacla <potel.clarence@gmail.com>
2023-07-11 01:01:45 +0200

    update event api

--- api/src/event/event.controller.ts
+++ api/src/event/event.controller.ts
@@ -1,17 +1,17 @@
 import { Body, Controller, Post, Req, ValidationPipe } from '@nestjs/common';
 import { ApiIdGuard, isPublic } from 'src/decorator';
-import { CreateEvent, Event, RequestWithUser } from 'src/models';
+import { CreateEventFromClient, Event, RequestWithUser } from 'src/models';
 import { EventService } from './event.service';
 
 @Controller({ path: 'event', version: '1' })
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
-  @Post()
+  @Post('front')
   @ApiIdGuard()
   @isPublic()
-  createEvent(@Req() req: RequestWithUser, @Body(ValidationPipe) createUserDto: CreateEvent) {
-    const { appID, ...data } = createUserDto;
+  createEvent(@Req() req: RequestWithUser, @Body(ValidationPipe) createEvent: CreateEventFromClient) {
+    const { APP_ID, ...data } = createEvent;
     const event: Partial<Event> = {
       user: req.user,
       ...data
