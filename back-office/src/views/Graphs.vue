9a13728c7c8f2dd796150be64073bbc883c4109d
Pierre <victsing@outlook.fr>
2023-07-16 15:28:36 +0200

    update components chartjs

--- back-office/src/views/Graphs.vue
+++ back-office/src/views/Graphs.vue
@@ -1,12 +1,23 @@
 <template>
-  <button @click="openForm">Open Form</button>
+  <div v-if="graphs.length === 0" class="text-center place-content-center" >
+    <div class="card-body">Pas de graphique</div>
+    <div><button>Créer votre graphique</button></div>
+  </div>
+
+  <div
+      v-else
+      v-for="(graph, index) in graphs"
+      :key="index"
+      class="text-center border border-base-content card bg-base-100"
+  >
+  </div>
   <PopupForm/>
 </template>
 
 
 <script lang="ts" setup>
 import {ref} from "vue";
-import type {GraphsModel} from "../models/viewModel/graphs.model";
+import type {GraphsModel} from "@/models/viewModel/graphs.model";
 import PopupForm from '../components/PopupForm.vue';
 
 const graphs = ref<GraphsModel[]>([]);
