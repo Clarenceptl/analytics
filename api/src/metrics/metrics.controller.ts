bbb7ff0fe694707da1a0b8d30c484d308aa13fda
Pierre <victsing@outlook.fr>
2023-07-16 23:25:22 +0200

    update metrics back-end

---
+++ api/src/metrics/metrics.controller.ts
@@ -0,0 +1,41 @@
+import {Body, Controller, Delete, Get, Param, Post, Put, Query} from '@nestjs/common';
+import { MetricsService } from './metrics.service';
+import { MetricsDto } from '../models/dto/metrics.dto';
+import { Metrics } from '../models/entities/metrics.entity';
+
+@Controller('metrics')
+export class MetricsController {
+  constructor(private readonly MetricsService: MetricsService) {}
+  @Post()
+  async create(@Body() createMetricsDto: MetricsDto): Promise<Metrics> {
+    return this.MetricsService.create(createMetricsDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Metrics[]> {
+    return this.MetricsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Metrics> {
+    return this.MetricsService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateMetricsDto: MetricsDto): Promise<Metrics> {
+    return this.MetricsService.update(id, updateMetricsDto);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Metrics> {
+    return this.MetricsService.delete(id);
+  }
+
+  @Get('query')
+  async findWithQuery(
+    @Query('dimensions') queryDimensions: string,
+    @Query('timeScale') timeScale: string
+  ): Promise<Metrics[]> {
+    return this.MetricsService.findWithQuery(queryDimensions, timeScale);
+  }
+}
---
+++ api/src/metrics/metrics.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Metrics } from '../models/entities/metrics.entity';
+import { MetricsDto } from '../models/dto/metrics.dto';
+import { CreateMetricsDto } from '../models/dto/create-metrics.dto';
+import { UpdateMetricsDto } from '../models/dto/update-metrics.dto';
+
+@Injectable()
+export class MetricsService {
+  constructor(@InjectModel('Metrics') private readonly metricsModels: Model<Metrics>) {}
+
+  async create(createMetricsDto: CreateMetricsDto): Promise<Metrics> {
+    const createdMetrics = new this.metricsModels(createMetricsDto);
+    return createdMetrics.save();
+  }
+
+  async findAll(): Promise<Metrics[]> {
+    return this.metricsModels.find().exec();
+  }
+
+  async findOne(id: string): Promise<Metrics> {
+    return this.metricsModels.findById(id).exec();
+  }
+
+  async update(id: string, updateMetricsDto: UpdateMetricsDto): Promise<Metrics> {
+    return this.metricsModels.findByIdAndUpdate(id, updateMetricsDto, { new: true }).exec();
+  }
+
+  async delete(id: string): Promise<Metrics> {
+    return this.metricsModels.findByIdAndRemove(id).exec();
+  }
+
+  async findWithQuery(queryDimensions: string, timeScale: string): Promise<Metrics[]> {
+    const parsedDimensions = JSON.parse(queryDimensions);
+    const parsedTimeScale = JSON.parse(timeScale);
+
+    const query = {
+      ...parsedDimensions, // Add conditions based on the parsed query dimensions
+      createdAt: {
+        $gte: new Date(parsedTimeScale.startTime),
+        $lte: new Date(parsedTimeScale.endTime)
+      }
+    };
+
+    return this.metricsModels.find(query).exec();
+  }
+}
